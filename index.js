2394810c4806b7e1d28830122cb752ece6d3b51c
Fatima salman <148888412+Jeonfatima@users.noreply.github.com>
2024-08-07 13:10:05 +0500

    Add files via upload
    Say goodbye to guesswork with my awesome currency converter project built with TypeScript! 🚀✨ Instantly convert money between various currencies with the latest exchange rates, right at your fingertips. 💸💹

---
+++ index.js
@@ -0,0 +1,33 @@
+import inquirer from 'inquirer';
+const currency = {
+    USD: 1, //Base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter current currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: "to",
+        message: "Enter the currency you'd like to convert into",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'amount',
+        message: 'Enter your amount',
+        type: 'number'
+    }
+]);
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let amount = userAnswer.amount;
+let baseAmount = amount / fromAmount;
+let converted_Amount = baseAmount * toAmount;
+console.log("Your converted Amount is :" + converted_Amount + userAnswer.to);
